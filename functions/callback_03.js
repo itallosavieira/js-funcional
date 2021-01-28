6a3f61108178137a2b614aa4b1c52f57be00e2c4
itallosavieira <itallo.sa.vieira@gmail.com>
2021-01-28 18:13:55 -0300

    .map

--- functions/callback_03.js
+++ functions/callback_03.js
@@ -1,46 +1,27 @@
-// const nums = [1, 2, 3, 4, 5];
-// const dobro = n => n * 2;
-// console.log(nums.map(dobro));
-
-
-
-const nomes = ['Rafa', 'Itallo', 'Leticia'];
-const primeiraLetra = t => t[0];
-console.log(nomes.map(primeiraLetra));
-
-
-
-const cart = [
-    { nome: 'Caneta', qtde: 10, preco: 7.99 },
-    { nome: 'Impressora', qtde: 0, preco: 600.80 },
-    { nome: 'Caderno', qtde: 7, preco: 28.10 },
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.90 },
+    { nome: 'Impressora', qtde: 0, preco: 600.90 },
+    { nome: 'Caderno', qtde: 2, preco: 19.20 }
 ]
 
-const getName = item => item.nome;
-console.log(cart.map(getName));
+const getName = item => item.nome
+console.log(carrinho.map(getName))
 
-const getTotal = item =>  item.qtde * item.preco;
-const totais = cart.map(getTotal);
-console.log(totais);
-
-//My own map
-const nums = [1, 2, 3, 4, 5];
-
-const dobro = n => n * 2;
-
-//const result = nums.map(dobro)
+const getTotal = item => item.qtde * item.preco
+const totais = carrinho.map(getTotal)
+console.log(totais)
 
 Array.prototype.myMap = function(fn) {
-    
-    const mapped = [];
+    let mapped = [];
 
-    for(let el of this) {
-        mapped.push(fn(el));
+    for (let el of this) {
+        mapped.push(fn(el))
     }
 
     return mapped;
-};
+}
 
-console.log(
-   nums.myMap(dobro)
-);
+const nums = [1, 2, 3, 4, 5]
+const dobro = n => n * 2
+const numerosDobrados = nums.myMap(dobro);
+console.log(numerosDobrados)
